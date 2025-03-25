297dc82781511d863c0758b891d668fa844ba9a3
muftimehedihasan <mehedihasan.projects@gmail.com>
2025-03-25 15:32:06 +0600

    Created DreamController and Routes for this.

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,29 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DreamController;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin/dreams', [DreamController::class, 'index'])->name('dreams.index');
+
+Route::get('/admin/dreams/{dream}', [DreamController::class, 'show'])->name('dreams.show');
+
+Route::get('/admin/dreams/create', [DreamController::class, 'create'])->name('dreams.create');
+
+Route::post('/admin/dreams/store', [DreamController::class, 'store'])->name('dreams.store');
+
+Route::get('/admin/dreams/{dream}/edit', [DreamController::class, 'edit'])->name('dreams.edit');
+
+Route::put('/admin/dreams/{dream}', [DreamController::class, 'update'])->name('dreams.update');
+
+Route::delete('/admin/dreams/{dream}/destroy', [DreamController::class, 'delete'])->name('dreams.destroy');
+
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
